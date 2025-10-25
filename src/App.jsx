e43ddd569934bc3936b91b6577c1a0e556b3898a
Natália  Ferreira <nataliaefernanda3054@gmail.com>
2025-10-25 17:12:53 -0300

    criando jogo da velha

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Game from "./game/Game";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Jogo da Velha</h1>
+      <Game />
+    </div>
+  );
+}
---
+++ src/game/Game.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Board from "./Board";
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+  }
+
+  const moves = history.map((squares, move) => {
+    const desc = move > 0 ? "Ir para o movimento #" + move : "Ir para o início do jogo";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
